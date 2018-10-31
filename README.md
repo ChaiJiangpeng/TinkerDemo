61c28c95b659d299b35dc7af0ce44cfc8b5704d5
ChaiJiangpeng <1156364937@qq.com>
2018-10-31 10:19:39 +0800

    提交兼容andresguard的module

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # TinkerDemo
 Tinker热更新demo
+
+andresguardtinker 为兼容使用AndResguard资源混淆
+
+请看[Android Tinker集成（含有AndResGuard资源混淆）](https://www.jianshu.com/p/f65f9f8e92e6)
---
+++ andresguardtinker/src/main/java/com/jiangpeng/andresguardtinker/NextActivity.java
@@ -0,0 +1,16 @@
+package com.jiangpeng.andresguardtinker;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by ChaiJiangPeng on 2018/10/23.
+ */
+public class NextActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_next);
+    }
+}
